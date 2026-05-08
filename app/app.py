a0e5c22efb48f3a05bd46095c3f3fd29c4b3bb9a
Kamal-Bhagchandani <kamal.m.bhagchandani@gmail.com>
2026-05-09 04:26:33 +0530

    Integrate CNN prediction model with Flask backend

--- app/app.py
+++ app/app.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+
+from training.predict import predict_fruit
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "app/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    # Get uploaded image
+    file = request.files["fruit_image"]
+
+    # Create file path
+    file_path = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        file.filename
+    )
+
+    # Save image temporarily
+    file.save(file_path)
+
+    # Predict fruit
+    fruit, confidence = predict_fruit(file_path)
+
+    # Delete image after prediction
+    os.remove(file_path)
+
+    # Render same page with prediction result
+    return render_template(
+        "index.html",
+        prediction=fruit,
+        confidence=f"{confidence:.2f}"
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
--- training/predict.py
+++ training/predict.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow.keras.preprocessing import image
 
 # Load trained model
-model = tf.keras.models.load_model("../model/fruit_model.h5")
+model = tf.keras.models.load_model("model/fruit_model.h5")
 
 # Class labels
 class_names = ["Apple", "Banana", "Grapes", "Mango"]
@@ -20,20 +20,13 @@ def predict_fruit(img_path):
     # Add batch dimension
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Predict
+    # Predict fruit
     predictions = model.predict(img_array)
 
-    # Get highest probability class
+    # Get predicted class
     predicted_class = class_names[np.argmax(predictions)]
 
     # Confidence score
     confidence = np.max(predictions) * 100
 
     return predicted_class, confidence
-
-
-# Test prediction
-fruit, confidence = predict_fruit("../sample_images/apple.jpg")
-
-print(f"Prediction: {fruit}")
-print(f"Confidence: {confidence:.2f}%")
