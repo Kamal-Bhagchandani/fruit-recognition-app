18265d5c43b1b7924cfde40dcd7d78945e18a8b0
Kamal-Bhagchandani <kamal.m.bhagchandani@gmail.com>
2026-05-11 14:56:59 +0530

    Enhance UI layout with note and loading feedback

--- app/static/script.js
+++ app/static/script.js
@@ -2,6 +2,13 @@ const imageInput = document.getElementById("fruit_image");
 
 const previewImage = document.getElementById("preview-image");
 
+const form = document.querySelector("form");
+
+form.addEventListener("submit", () => {
+    const button = document.querySelector("button");
+    button.innerText = "Predicting...";
+});
+
 imageInput.addEventListener("change", function(event) {
     const file = event.target.files[0];
 
